a856149758df2e1b8a2afb52821b66b14fd2fa4e
Alexandr Starodubenko <alexvladstr@gmail.com>
2023-07-09 01:52:26 +0300

    Set up project

---
+++ src/main/java/ru/starodub/voting/RestaurantVotingApp.java
@@ -0,0 +1,11 @@
+package ru.starodub.voting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApp.class, args);
+    }
+}
---
+++ src/test/java/ru/starodub/voting/RestaurantVotingAppTest.java
@@ -0,0 +1,13 @@
+package ru.starodub.voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RestaurantVotingAppTest {
+    @Test
+    public void isContextLoad() {
+    }
+}
